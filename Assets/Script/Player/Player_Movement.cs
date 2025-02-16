485776ff493c3dd78428a3b34d318b7fe4c84a96
Obito0979 <baudopippo04@gmail.com>
2025-02-16 22:49:35 +0100

    Add scene
    add scene

--- Assets/Script/Player/Player_Movement.cs
+++ Assets/Script/Player/Player_Movement.cs
@@ -2,31 +2,61 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
+    // Velocita movimento
     public float speed = 5;
+
+    // gameobject di player
     [SerializeField] private GameObject player;
+
+    // SpriteRendere di player 
     private SpriteRenderer spr;
+
+    // Animazione player
+    private Animator anim;
+    private string WALK_ANIMATION = "Walk";
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Awake() {
-        spr = player.GetComponent<SpriteRenderer>();    
+    void Awake()
+    {
+        spr = player.GetComponent<SpriteRenderer>();
+        anim = player.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        // Movement
         float x = Input.GetAxisRaw("Horizontal") * speed * Time.deltaTime;
         float y = Input.GetAxisRaw("Vertical") * speed * Time.deltaTime;
 
         player.transform.position += new Vector3(x, y, 0);
-        if (x > 0)
-        {
-            spr.flipX = false;
-        } else if (x < 0)
-        {
-            spr.flipX = true;
-        }
+
+        // Walk animation
+        Animation_Walk_Player(x, y);
 
     }
 
-    
+    void Animation_Walk_Player(float x, float y)
+    {
+        if (x > 0)
+        {
+            spr.flipX = false;
+            anim.SetBool(WALK_ANIMATION, true);
+        }
+        else if (x < 0)
+        {
+            spr.flipX = true;
+            anim.SetBool(WALK_ANIMATION, true);
+        }
+        else if (y != 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+        }
+        else
+        {
+            anim.SetBool(WALK_ANIMATION, false);
+        }
+    }
+
 
 }
